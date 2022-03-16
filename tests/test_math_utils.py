50f528dd46c566c0215c9b8797fe7668054f13cc
jonathanoesterle <jonathan.oesterle1@gmail.com>
2022-03-16 09:58:00 +0100

    Fixed test

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -2,15 +2,15 @@ import numpy as np
 from djimaging.utils import math_utils
 
 
-def normalize_zero_one_1darray():
+def test_normalize_zero_one_1darray():
     x = np.array([1., 2./3., 7.5, np.pi])
     obs = math_utils.normalize_zero_one(x)
     assert np.isclose(np.max(obs), 1.)
     assert np.isclose(np.min(obs), 0.)
 
 
-def normalize_zero_one_2darray():
-    x = np.array([1., 2., 7.5, np.pi], [4.333, 2.15423, 7.5, 3.])
+def test_normalize_zero_one_2darray():
+    x = np.array([[1., 2., 7.5, np.pi], [4.333, 2.15423, 7.5, 3.]])
     obs = math_utils.normalize_zero_one(x)
     assert np.isclose(np.max(obs), 1.)
     assert np.isclose(np.min(obs), 0.)
