7c2c9f30e06ffafff80514fdbcdb62da2ff86c56
jonathanoesterle <jonathan.oesterle1@gmail.com>
2022-03-16 16:17:01 +0100

    Added on_off_tests

--- djimaging/basic_tables/chirp.py
+++ djimaging/basic_tables/chirp.py
@@ -33,6 +33,81 @@ class ChirpQITemplate(dj.Computed):
         self.insert1(dict(key, chirp_qi=chirp_qi, min_qi=min_qi))
 
 
+def _compute_on_off_index(snippets, snippets_times, trigger_times, sf, light_step_duration=1):
+    start_trigs = trigger_times[::2]
+    light_step_frames = int(light_step_duration * sf)
+
+    on_responses = np.zeros((light_step_frames, snippets.shape[1]))
+    off_responses = np.zeros((light_step_frames, snippets.shape[1]))
+
+    for i in range(snippets.shape[1]):
+        step_up = start_trigs[i] + 2
+        step_down = start_trigs[i] + 5
+
+        snip_times = snippets_times[:, i]
+        snip = snippets[:, i]
+        snip = snip - snip.min()
+
+        step_up_idx = np.argmax(np.isclose(snip_times, step_up, atol=1e-01))
+        step_down_idx = np.argmax(np.isclose(snip_times, step_down, atol=1e-01))
+
+        baseline_on = np.median(snip[:step_up_idx])
+        on_response = snip[step_up_idx:step_up_idx + light_step_frames] - baseline_on
+        on_response[on_response < 0] = 0
+
+        baseline_off = np.median(snip[step_down_idx - light_step_frames:step_down_idx])
+        off_response = snip[step_down_idx:step_down_idx + light_step_frames] - baseline_off
+        off_response[off_response < 0] = 0
+
+        on_responses[:, i] = on_response
+        off_responses[:, i] = off_response
+
+    avg_on = on_responses[4:, :].mean()
+    avg_off = off_responses[4:, :].mean()
+
+    if avg_on + avg_off < 1e-10:
+        on_off_index = 0
+    else:
+        on_off_index = (avg_on - avg_off) / (avg_on + avg_off)
+
+    return on_off_index
+
+
+def _compute_transcience_index(snippets, snippets_times, trigger_times, sf, upsam_fre=500):
+    upsampling_factor = int(snippets.shape[0] / sf * upsam_fre)
+
+    resampled_snippets = np.zeros((upsampling_factor, snippets.shape[1]))
+    resampled_snippets_times = np.zeros((upsampling_factor, snippets.shape[1]))
+
+    for i in range(snippets.shape[1]):
+        resampled_snippets[:, i] = signal.resample(snippets[:, i], int(upsampling_factor))
+
+        start_time = snippets_times[0, i]
+        end_time = snippets_times[-1, i]
+        resampled_snippets_times[:, i] = np.linspace(start_time, end_time, upsampling_factor)
+        # used this as interpolartion, signal.resample had edge effects when interpolaring the linear array
+
+    # Local Chirp
+    start_trigs_local = trigger_times[::2]
+
+    RTi = np.zeros(snippets.shape[1])
+    for i in range(snippets.shape[1]):
+        stim_start = start_trigs_local[i]
+        stim_end = start_trigs_local[i] + 6
+        snip_times = resampled_snippets_times[:, i]
+        snip = resampled_snippets[:, i]
+        snip = snip - snip.min()
+        stim_start_idx = np.argmax(np.isclose(snip_times, stim_start, atol=1e-01))
+        stim_end_idx = np.argmax(np.isclose(snip_times, stim_end, atol=1e-01))
+        trace = snip[stim_start_idx:stim_end_idx]
+        peak = np.argmax(trace)
+        peak_alpha = np.argmax(np.isclose(snip_times, snip_times[peak] + 0.4, atol=1e-01))
+        RTi[i] = 1 - (snip[stim_start_idx + peak_alpha] / snip[stim_start_idx + peak])
+    RTi = RTi.mean()
+
+    return RTi
+
+
 class ChirpFeaturesTemplate(dj.Computed):
     # TODO: Add more features
 
@@ -57,79 +132,13 @@ class ChirpFeaturesTemplate(dj.Computed):
         return self.detrendsnippets_table() & "stim_id=1"
 
     def make(self, key):
-        # TODO: Make this code readable
         # TODO: Should this depend on pres? Triggertimes are also in snippets and sf can be derived from times
         snippets = (self.detrendsnippets_table() & key).fetch1('detrend_snippets')
         snippets_times = (self.detrendsnippets_table() & key).fetch1('snippets_times')
         trigger_times = (self.presentation_table() & key).fetch1('triggertimes')
         sf = (self.presentation_table() & key).fetch1('scan_frequency')
-        start_trigs = trigger_times[::2]
-        light_step_duration = 1
-        light_step_frames = int(light_step_duration * sf)
 
-        # On-Off Index
-        on_responses = np.zeros((light_step_frames, snippets.shape[1]))
-        off_responses = np.zeros((light_step_frames, snippets.shape[1]))
+        on_off_index = _compute_on_off_index(snippets, snippets_times, trigger_times, sf)
+        transience_index = _compute_transcience_index(snippets, snippets_times, trigger_times, sf)
 
-        for i in range(snippets.shape[1]):
-            step_up = start_trigs[i] + 2
-            snip_times = snippets_times[:, i]
-            snip = snippets[:, i]
-            snip = snip - snip.min()
-            step_up_idx = np.argmax(np.isclose(snip_times, step_up, atol=1e-01))
-            step_down = start_trigs[i] + 5
-            step_down_idx = np.argmax(np.isclose(snip_times, step_down, atol=1e-01))
-            baseline_on = np.median(snip[:step_up_idx])
-            on_response = snip[step_up_idx:step_up_idx + light_step_frames]
-            off_response = \
-                snip[step_down_idx:step_down_idx + light_step_frames]
-            on_response = on_response - baseline_on
-            on_response[on_response < 0] = 0
-            baseline_off = np.median(snip[step_down_idx - light_step_frames:step_down_idx])
-            off_response = off_response - baseline_off
-            off_response[off_response < 0] = 0
-            on_responses[:, i] = on_response
-            off_responses[:, i] = off_response
-
-        avg_on = on_responses[4:, :].mean()
-        avg_off = off_responses[4:, :].mean()
-
-        if avg_on + avg_off < 1e-10:
-            on_off_index = 0
-        else:
-            on_off_index = (avg_on - avg_off) / (avg_on + avg_off)
-
-        # Transience Index
-        upsam_fre = 500
-        upsampling_factor = int(snippets.shape[0] / sf * upsam_fre)
-
-        resampled_snippets = np.zeros((upsampling_factor, snippets.shape[1]))
-        resampled_snippets_times = np.zeros((upsampling_factor, snippets.shape[1]))
-
-        for i in range(snippets.shape[1]):
-            resampled_snippets[:, i] = signal.resample(snippets[:, i], int(upsampling_factor))
-
-            start_time = snippets_times[0, i]
-            end_time = snippets_times[-1, i]
-            resampled_snippets_times[:, i] = np.linspace(start_time, end_time, upsampling_factor)
-            # used this as interpolartion, signal.resample had edge effects when interpolaring the linear array
-
-        # Local Chirp
-        start_trigs_local = trigger_times[::2]
-
-        RTi = np.zeros(snippets.shape[1])
-        for i in range(snippets.shape[1]):
-            stim_start = start_trigs_local[i]
-            stim_end = start_trigs_local[i] + 6
-            snip_times = resampled_snippets_times[:, i]
-            snip = resampled_snippets[:, i]
-            snip = snip - snip.min()
-            stim_start_idx = np.argmax(np.isclose(snip_times, stim_start, atol=1e-01))
-            stim_end_idx = np.argmax(np.isclose(snip_times, stim_end, atol=1e-01))
-            trace = snip[stim_start_idx:stim_end_idx]
-            peak = np.where(trace == trace.max())[0][0]
-            peak_alpha = np.nonzero(np.isclose(snip_times, snip_times[peak] + 0.4, atol=1e-01))[0][0]
-            RTi[i] = 1 - (snip[stim_start_idx + peak_alpha] / snip[stim_start_idx + peak])
-        RTi = RTi.mean()
-
-        self.insert1(dict(key, on_off_index=on_off_index, transience_index=RTi))
+        self.insert1(dict(key, on_off_index=on_off_index, transience_index=transience_index))
---
+++ tests/test_chirp.py
@@ -0,0 +1,79 @@
+from djimaging.basic_tables.chirp import _compute_on_off_index
+import numpy as np
+
+
+def test_compute_on_off_index_on_response():
+    t_start = 0.0
+    n_reps = 3
+    sf = 7.81
+    stim_dur = sf * 4.2
+    sin_f = 0.2
+
+    np.random.seed(42)
+    frames_per_rep = int(np.floor(stim_dur * sf))
+
+    # Create data
+    trace_times = np.arange(frames_per_rep) * 1 / sf
+    trace = np.sin(sin_f * (trace_times - 2) * np.pi * 2)
+
+    # Make on response
+    trace[(trace_times < 2) | (trace_times > 2 + 0.5 / sin_f)] = 0
+    trace_times += t_start
+
+    snippets_times = np.concatenate([trace_times + i * stim_dur for i in range(n_reps)]).reshape(n_reps, -1).T
+    snippets = np.tile(trace, (n_reps, 1)).T
+
+    trigger_times = np.concatenate([[t_start + i * stim_dur, t_start + i * stim_dur] for i in range(n_reps)])
+
+    obs = _compute_on_off_index(snippets, snippets_times, trigger_times, sf, light_step_duration=1)
+    exp_lb = 0.95
+    assert obs > exp_lb
+
+
+def test_compute_on_off_index_off_response():
+    t_start = 0.0
+    n_reps = 3
+    sf = 7.81
+    stim_dur = sf * 4.2
+    sin_f = 0.2
+
+    np.random.seed(42)
+    frames_per_rep = int(np.floor(stim_dur * sf))
+
+    # Create data
+    trace_times = np.arange(frames_per_rep) * 1 / sf
+
+    response_at_t = 5
+    trace = np.sin(sin_f * (trace_times - response_at_t) * np.pi * 2)
+    trace[(trace_times < response_at_t) | (trace_times > response_at_t + 0.5 / sin_f)] = 0
+    trace_times += t_start
+
+    snippets_times = np.concatenate([trace_times + i * stim_dur for i in range(n_reps)]).reshape(n_reps, -1).T
+    snippets = np.tile(trace, (n_reps, 1)).T
+
+    trigger_times = np.concatenate([[t_start + i * stim_dur, t_start + i * stim_dur] for i in range(n_reps)])
+
+    obs = _compute_on_off_index(snippets, snippets_times, trigger_times, sf, light_step_duration=1)
+    exp_ub = -0.95
+    assert obs < exp_ub
+
+
+def test_compute_on_off_index_no_response():
+    t_start = 0.0
+    n_reps = 3
+    sf = 7.81
+    stim_dur = sf * 4.2
+
+    np.random.seed(42)
+    frames_per_rep = int(np.floor(stim_dur * sf))
+
+    # Create data
+    trace_times = np.arange(frames_per_rep) * 1 / sf
+    snippets_times = np.concatenate([trace_times + i * stim_dur for i in range(n_reps)]).reshape(n_reps, -1).T
+    snippets = np.zeros(snippets_times.shape)
+
+    trigger_times = np.concatenate([[t_start + i * stim_dur, t_start + i * stim_dur] for i in range(n_reps)])
+
+    obs = _compute_on_off_index(snippets, snippets_times, trigger_times, sf, light_step_duration=1)
+    exp = 0.0
+    assert obs == exp
