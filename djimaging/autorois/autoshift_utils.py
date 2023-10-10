2472ce0f55392e5e07d7d2364f5fe17de78b5ea3
Jonathan Oesterle <jonathan.oesterle.work@gmail.com>
2023-10-10 13:51:30 +0200

    Add autoshift utils.

---
+++ djimaging/autorois/autoshift_utils.py
@@ -0,0 +1,131 @@
+import numpy as np
+
+
+def compute_corr_map(stack):
+    """
+    Calculate the local neighboring pixel correlation matrix for a 3D stack (x, y, t).
+
+    Input:
+    stack (numpy.ndarray): 3D spatiotemporal data, the first two dimensions (nx, ny) correspond to spatial dimensions,
+    and the third dimension (_) represents the time series.
+
+    Output:
+    corr_map (numpy.ndarray): 2D array with normalized cross-correlations for each pixel in the spatial domain.
+    The values represent the local similarity of each pixel to its neighbors within predefined shifts.
+    """
+    nx, ny, _ = stack.shape
+    corr_map = np.zeros((nx, ny))
+    counts = np.zeros((nx, ny))
+
+    for ix in range(nx):
+        for iy in range(ny):
+            for idx, idy in [(0, 1), (1, 0), (1, 1), (0, -1), (-1, 0), (-1, -1), (-1, 1), (1, -1)]:
+                if ix + idx >= nx or iy + idy >= ny or ix + idx < 0 or iy + idy < 0:
+                    continue
+                else:
+                    counts[ix, iy] += 1
+                    corr_map[ix, iy] += np.corrcoef(stack[ix + idx, iy + idy, :], stack[ix, iy, :])[0, 1]
+
+    # Normalize by the number of neighbors
+    corr_map /= counts
+
+    return corr_map
+
+
+def compute_corr_map_match_indexes(corr_map, ref_corr_map, shift_max=20, metric='mse', verbose=False,
+                                   plot=False):
+    """Calculate the match_indexes between a stack_corr image and its shifted versions.
+
+    Input:
+    shifted_video (numpy.ndarray): input video as 3D numpy array (frames, height, width).
+    stack_corr (numpy.ndarray): 2D reference image for cross-correlation.
+    shift_max (int): maximum shift value in both x and y directions.
+
+    Output:
+    corr_map (array): cross-correlation matrix with shape (2*shift_max+1, 2*shift_max+1).
+    """
+
+    # Initialize the cross-correlation matrix with zeros
+    match_indexes = np.zeros((2 * shift_max + 1, 2 * shift_max + 1))
+
+    # Iterate over all possible shifts in both x and y directions within the range [-ShiftMax, ShiftMax]
+    for dx in range(-shift_max, shift_max + 1):
+        for dy in range(-shift_max, shift_max + 1):
+            # Crop the shifted_video and stack_corr images to the same size
+            cropped = corr_map[max(0, -dx):min(ref_corr_map.shape[0], ref_corr_map.shape[0] - dx),
+                      max(0, -dy):min(ref_corr_map.shape[1], ref_corr_map.shape[1] - dy)]
+
+            ref_cropped = ref_corr_map[max(0, dx):min(ref_corr_map.shape[0], ref_corr_map.shape[0] + dx),
+                          max(0, dy):min(ref_corr_map.shape[1], ref_corr_map.shape[1] + dy)]
+
+            # Calculate the correlation coefficient between the cropped images
+            if metric == 'corr':
+                score = np.corrcoef(cropped.flatten(), ref_cropped.flatten())[0, 1]
+            elif metric == 'mse':
+                score = -np.mean((cropped - ref_cropped) ** 2)
+            else:
+                raise NotImplementedError(f"Metric {metric} not implemented")
+
+            if plot:
+                from matplotlib import pyplot as plt
+                fig = plt.figure(figsize=(6, 6))
+                fig.suptitle(f"dx={dx}, dy={dy}, {metric}={score:.2f}")
+
+                ax1 = fig.add_subplot(2, 2, 1)
+                ax1.imshow(ref_cropped)
+
+                ax2 = fig.add_subplot(2, 2, 2)
+                ax2.imshow(cropped)
+
+                plt.show()
+
+            # Store the correlation coefficient in the cross-correlation matrix
+            match_indexes[dx + shift_max, dy + shift_max] = score
+
+    # Calculate image statistics
+    v_max = np.max(match_indexes)
+    v_max_row_loc, v_max_col_loc = np.unravel_index(np.argmax(match_indexes), match_indexes.shape)
+
+    # Shift the ROI mask
+    x_shift = v_max_row_loc - shift_max
+    y_shift = v_max_col_loc - shift_max
+
+    if verbose:
+        print("ROI mask shifted by", x_shift, "pixels in x and", y_shift, "pixels in y")
+        print("Maximum correlation:", v_max)
+
+    return match_indexes
+
+
+def extract_best_shift(match_indexes):
+    # Calculate image statistics
+    rel_x_max, rel_y_max = np.unravel_index(np.argmax(match_indexes), match_indexes.shape)
+
+    # Calculate the expected shift values
+    shift_x = ((match_indexes.shape[0] - 1) // 2) - rel_x_max
+    shift_y = ((match_indexes.shape[1] - 1) // 2) - rel_y_max
+
+    return shift_x, shift_y
+
+
+def shift_img(img, shift_x, shift_y, fun_cval=np.median):
+    shifted_img = np.roll(img, shift=shift_x, axis=0)
+    shifted_img = np.roll(shifted_img, shift=shift_y, axis=1)
+
+    cval = fun_cval(img)
+
+    if shift_x > 0:
+        shifted_img[:shift_x, :] = cval
+    elif shift_x < 0:
+        shifted_img[shift_x:, :] = cval
+    else:
+        pass
+
+    if shift_y > 0:
+        shifted_img[:, :shift_y] = cval
+    elif shift_y < 0:
+        shifted_img[:, shift_y:] = cval
+    else:
+        pass
+
+    return shifted_img
---
+++ tests/autorois/test_autoshift.py
@@ -0,0 +1,92 @@
+import os.path
+
+import numpy as np
+import pytest
+from djimaging.utils import scanm_utils
+
+from djimaging.autorois.autoshift_utils import compute_corr_map, compute_corr_map_match_indexes, \
+    shift_img, extract_best_shift
+
+params_test_shit_img = [
+    (0, 0),
+    (1, 0),
+    (0, 1),
+    (1, 1),
+    (-1, 0),
+    (0, -1),
+    (-1, -1),
+    (1, -1),
+    (-1, 1),
+]
+
+
+@pytest.mark.parametrize("shift_x, shift_y", params_test_shit_img)
+def test_shit_img(shift_x, shift_y):
+    img = np.random.randint(0, 100, (3, 5))
+    shifted_img = shift_img(img, shift_x=shift_x, shift_y=shift_y)
+
+    assert img.shape == shifted_img.shape
+
+    if shift_x > 0:
+        crop = img[:-shift_x, :]
+        shifted_crop = shifted_img[shift_x:, :]
+    elif shift_x < 0:
+        crop = img[-shift_x:, :]
+        shifted_crop = shifted_img[:shift_x, :]
+    else:
+        crop = img
+        shifted_crop = shifted_img
+
+    assert crop.shape == shifted_crop.shape
+
+    if shift_y > 0:
+        crop = crop[:, :-shift_y]
+        shifted_crop = shifted_crop[:, shift_y:]
+    elif shift_y < 0:
+        crop = crop[:, -shift_y:]
+        shifted_crop = shifted_crop[:, :shift_y]
+    else:
+        pass
+
+    assert crop.shape == shifted_crop.shape
+    assert np.all(crop == shifted_crop)
+
+
+filename_stack1 = '/gpfs01/euler/data/Data/DataJointTestData/xy-RGCs/20220125/2/Pre/SMP_M1_RR_GCL0_chirp_C1.h5'
+
+if os.path.isfile(filename_stack1):
+    ch_stacks, wparams = scanm_utils.load_stacks_from_h5(filename_stack1, ch_names=('wDataCh0',))
+    stack1 = ch_stacks['wDataCh0']
+else:
+    stack1 = None
+
+params_test_autoshift_real_stacks = [
+    (0, -1, 3, stack1, 'corr'),
+    (-1, -1, 3, stack1, 'corr'),
+    (-2, 2, 5, stack1, 'corr'),
+    (0, 0, 3, stack1, 'mse'),
+    (1, 0, 3, stack1, 'mse'),
+    (0, 1, 3, stack1, 'mse'),
+    (1, 1, 3, stack1, 'mse'),
+    (-1, 0, 3, stack1, 'mse'),
+    (0, -1, 3, stack1, 'mse'),
+    (-1, -1, 3, stack1, 'mse'),
+    (-2, 2, 5, stack1, 'mse'),
+]
+
+
+@pytest.mark.parametrize("shift_x, shift_y, shift_max, stack, metric", params_test_autoshift_real_stacks)
+@pytest.mark.skipif(stack1 is None, reason="No stack1 file found")
+def test_autoshift_real_stacks(shift_x, shift_y, shift_max, stack, metric):
+    # Calculate local cross-correlation matrix for reference
+    ref_corr = compute_corr_map(stack)
+
+    # Introduce shift
+    shifted_corr = shift_img(ref_corr, shift_x, shift_y)
+
+    # Calculate the cross-correlation matrix between shifted and reference images
+    match_indexes = compute_corr_map_match_indexes(shifted_corr, ref_corr, shift_max)
+
+    obs_shift_x, obs_shift_y = extract_best_shift(match_indexes)
+
+    assert obs_shift_x == shift_x and obs_shift_y == shift_y
