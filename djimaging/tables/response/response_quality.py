4f3e8d2b3d83724c56375ca4adb581a8fcb54ee1
Jonathan Oesterle <jonathan.oesterle.work@gmail.com>
2023-12-22 11:11:06 +0100

    Add Presentation Quality table.

--- djimaging/tables/response/response_quality.py
+++ djimaging/tables/response/response_quality.py
@@ -83,19 +83,43 @@ class RepeatQITemplate(dj.Computed):
 
 
 class RepeatQIPresentationTemplate(dj.Computed):
+    """
+    Computes statistics of the quality index for all ROIs in one presentation.
+
+    Example usage:
+    @schema
+    class ChirpQIPresentation(response.RepeatQIPresentationTemplate):
+        _qidx_col = 'qidx'
+        _levels = (0.25, 0.3, 0.35, 0.4, 0.45)
+        presentation_table = Presentation
+        qi_table = ChirpQI
+
+    @schema
+    class BarQIPresentation(response.RepeatQIPresentationTemplate):
+        _qidx_col = 'd_qi'
+        _levels = (0.5, 0.6, 0.7)
+        presentation_table = Presentation
+        qi_table = OsDsIndexes
+    """
+
     database = ""
+    _qidx_col = 'qidx'  # name of the column in the QI table
+    _levels = (0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6)  # levels for fraction of ROIs with qidx >= level
 
     @property
     def definition(self):
-        definition = f'''
+        definition = '''
         # Summarizes Quality Indexes for all ROIs in one presentation 
         -> self.presentation_table
-        -> self.qi_table
         ---
-        mean_qidx: float   # mean quality index
-        min_qidx: float   # mean quality index
-        max_qidx: float   # mean quality index
+        p_qidx_n_rois : int  # Number of ROIs
+        p_qidx_median: float  # median quality index
+        p_qidx_mean: float   # mean quality index
+        p_qidx_min: float   # min quality index
+        p_qidx_max: float   # max quality index
         '''
+        for level in self._levels:
+            definition += f'p_qidx_f{int(level * 100):03d}: float   # fraction of ROIs with qidx >= {level}\n'
         return definition
 
     @property
@@ -107,3 +131,25 @@ class RepeatQIPresentationTemplate(dj.Computed):
     @abstractmethod
     def qi_table(self):
         pass
+
+    @property
+    def key_source(self):
+        try:
+            return self.presentation_table().proj() & self.qi_table().proj()
+        except (AttributeError, TypeError):
+            pass
+
+    def make(self, key):
+        qidxs = (self.qi_table() & key).fetch(self._qidx_col)
+        new_key = dict(
+            key,
+            p_qidx_n_rois=len(qidxs),
+            p_qidx_mean=np.mean(qidxs),
+            p_qidx_median=np.median(qidxs),
+            p_qidx_min=np.min(qidxs),
+            p_qidx_max=np.max(qidxs),
+        )
+        for level in self._levels:
+            new_key[f'p_qidx_f{int(level * 100):03d}'] = np.mean(qidxs >= level)
+
+        self.insert1(new_key)
