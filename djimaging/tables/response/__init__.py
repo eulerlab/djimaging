643502b3d8309f4fe732d08ed87fede3b8831c48
Jonathan Oesterle <jonathan.oesterle.work@gmail.com>
2024-02-29 15:04:47 +0100

    Improve CSL processing.

--- djimaging/tables/response/__init__.py
+++ djimaging/tables/response/__init__.py
@@ -1,6 +1,6 @@
 from .chirp import ChirpQITemplate, ChirpFeaturesTemplate
 from .orientation import OsDsIndexesTemplate
 from .sinespot import SineSpotQITemplate, SineSpotFeaturesTemplate
-from .csl import CslSlopeTemplate, CslFeaturesTemplate
+from .csl import CslMetricsTemplate
 from .response_quality import RepeatQITemplate
 from .presentation_quality import RepeatQIPresentationTemplate
