ae6a18de5743714b9fe3e616ee76c56fa970bfee
Jonathan Oesterle <jonathan.oesterle.work@gmail.com>
2024-04-12 14:28:59 +0200

    Add chirp surround table.

--- djimaging/tables/response/__init__.py
+++ djimaging/tables/response/__init__.py
@@ -4,3 +4,4 @@ from .sinespot import SineSpotQITemplate, SineSpotFeaturesTemplate
 from .csl import CslMetricsTemplate
 from .response_quality import RepeatQITemplate
 from .presentation_quality import RepeatQIPresentationTemplate
+from .chirp_surround import ChirpSurroundTemplate
